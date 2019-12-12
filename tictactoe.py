f1b3c6ef04fe757f8d4f3a7bf743bfb98294f7a6
Marethyu12 <42195546+Marethyu12@users.noreply.github.com>
2019-12-12 11:16:13 -0800

    Completed Board class in tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,20 +1,19 @@
 class Board:
     valid_ch = ["X", "O", "_"]
     
-    def __init__(self, data=[[["_" * 3]] for i in range(0, 3)]):
+    def __init__(self, data=[["_"] * 3 for i in range(0, 3)]):
         self.data = data
         self._check()
     
     def set(self, row, col, ch):
-        if ch not in valid_ch:
+        if ch not in self.valid_ch:
             raise ValueError("ch is invalid")
         
         self.data[row][col] = ch
     
     def winner(self):
         if ((self.data[0][0] == self.data[1][1] and self.data[1][1] == self.data[2][2]) or
-            (self.data[2][0] == self.data[1][1] and self.data[1][1] == self.data[0][2])) and
-            self.data[1][1] is not "_":
+            (self.data[2][0] == self.data[1][1] and self.data[1][1] == self.data[0][2])) and self.data[1][1] is not "_":
             return self.data[1][1]
         
         for i in range(0, 3):
@@ -25,7 +24,14 @@ class Board:
             if self.data[0][j] == self.data[1][j] and self.data[1][j] == self.data[2][j] and self.data[0][j] is not "_":
                 return self.data[0][j]
         
-        return "T"
+        b = True
+        
+        for i in range(0, 3):
+            for j in range(0, 3):
+                if self.data[i][j] == "_":
+                    b = False
+        
+        return "T" if b else "N"
     
     def score(self, player):
         winner = self.winner()
@@ -38,8 +44,13 @@ class Board:
             return 0
     
     def __str__(self):
-        return ("-------------"
-                "| " + self.data[0][0] + " | " + self.data[0][1] + " | " + self.data[0][2] + " |") # todo
+        return ("-------------\n"
+                "| " + self.data[0][0] + " | " + self.data[0][1] + " | " + self.data[0][2] + " |\n"
+                "-------------\n"
+                "| " + self.data[1][0] + " | " + self.data[1][1] + " | " + self.data[1][2] + " |\n"
+                "-------------\n"
+                "| " + self.data[2][0] + " | " + self.data[2][1] + " | " + self.data[2][2] + " |\n"
+                "-------------\n")
     
     def _check(self):
         if len(self.data) is not 3 or len(self.data[0]) is not 3:
@@ -47,13 +58,13 @@ class Board:
         
         for i in range(0, 3):
             for j in range(0, 3):
-                if self.data[i][j] not in valid_ch:
+                if self.data[i][j] not in self.valid_ch:
                     raise ValueError("The one of characters in data is not recognized")
     
     def _enemy(self, player):
         if player is "X":
-            return "O":
+            return "O"
         elif player is "O":
-            return "X":
+            return "X"
         else:
             raise ValueError("Unknown player (_ or something)")
