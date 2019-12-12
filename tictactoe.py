2624c13be183f555ebe7f6b6f61f1516435efa61
Marethyu12 <42195546+Marethyu12@users.noreply.github.com>
2019-12-12 10:51:58 -0800

    Updated tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,22 +1,29 @@
 class Board:
-    def __init__(self, mat):
-        self.mat = mat
+    valid_ch = ["X", "O", "_"]
+    
+    def __init__(self, data=[[["_" * 3]] for i in range(0, 3)]):
+        self.data = data
+        self._check()
+    
+    def set(self, row, col, ch):
+        if ch not in valid_ch:
+            raise ValueError("ch is invalid")
         
-        if len(mat) is not 3 or len(mat[0]) is not 3:
-            raise ValueError("mat isn't a 3x3 matrix")
+        self.data[row][col] = ch
     
     def winner(self):
-        if self.mat[0][0] == self.mat[1][1] and self.mat[1][1] == self.mat[2][2] or
-           self.mat[2][0] == self.mat[1][1] and self.mat[0][2]:
-            return self.mat[1][1]
+        if ((self.data[0][0] == self.data[1][1] and self.data[1][1] == self.data[2][2]) or
+            (self.data[2][0] == self.data[1][1] and self.data[1][1] == self.data[0][2])) and
+            self.data[1][1] is not "_":
+            return self.data[1][1]
         
         for i in range(0, 3):
-            if self.mat[i][0] == self.mat[i][1] and self.mat[i][1] == self.mat[i][2]:
-                return self.mat[i][0]
+            if self.data[i][0] == self.data[i][1] and self.data[i][1] == self.data[i][2] and self.data[i][0] is not "_":
+                return self.data[i][0]
         
         for j in range(0, 3):
-            if self.mat[0][j] == self.mat[1][j] and self.mat[1][j] == self.mat[2][j]:
-                return self.mat[0][j]
+            if self.data[0][j] == self.data[1][j] and self.data[1][j] == self.data[2][j] and self.data[0][j] is not "_":
+                return self.data[0][j]
         
         return "T"
     
@@ -25,13 +32,28 @@ class Board:
         
         if winner is player:
             return 10
-        elif winner
-    # todo: add set method, handle _, and to_str
+        elif winner is self._enemy(player):
+            return -10
+        else:
+            return 0
+    
+    def __str__(self):
+        return ("-------------"
+                "| " + self.data[0][0] + " | " + self.data[0][1] + " | " + self.data[0][2] + " |") # todo
+    
+    def _check(self):
+        if len(self.data) is not 3 or len(self.data[0]) is not 3:
+            raise ValueError("data isn't a 3x3 matrix")
+        
+        for i in range(0, 3):
+            for j in range(0, 3):
+                if self.data[i][j] not in valid_ch:
+                    raise ValueError("The one of characters in data is not recognized")
+    
     def _enemy(self, player):
         if player is "X":
             return "O":
         elif player is "O":
             return "X":
         else:
-            raise ValueError("Unknown player")
-
+            raise ValueError("Unknown player (_ or something)")
