f823779319d5a967659ce791c3100e71230e78c1
marethyu12 <codingexpert123@gmail.com>
2019-12-11 16:58:05 -0800

    Updated bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 
 cmd_prefix = "!masq "
 token = "NjU0MTQzNzM1ODgxNDAwMzIx.XfBQ5A.dvoavyYMFuzb5J9Jp3e3-r0AR7E"
+jimmys_id = 489987032920358922
 
 client = commands.Bot(command_prefix=cmd_prefix)
 
@@ -17,18 +18,16 @@ async def intro(ctx):
     intro_msg = ("Hi I am Masquerade a general-purpose Discord bot. I am maintained and developed by JY.\n\n"
                  "**Usage:** !masq [command] [arguments]\n\n"
                  "**List of commands:**\n"
-                 "`hi` Sends me a 'hi' message."
-                 "`intro' Introduces myself"
+                 "`hi` Sends me a 'hi' message\n"
+                 "`intro' Introduces myself\n"
                  "`potassium` (Only JY can use it) Make bot offline")
     
     await ctx.send(intro_msg)
 
 @client.command(pass_context=True)
 async def potassium(ctx):
-    if ctx.message.author.id is not "489987032920358922":
-        return
-    
-    await client.logout()
+    if ctx.message.author.id == jimmys_id:
+        await client.logout()
 
 @client.event
 async def on_ready():
